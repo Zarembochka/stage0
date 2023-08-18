c46ec3ddcd0050e63cab95bfe227b743c70b70fe
Liubou Hadunova <Luba.godunova2018@gmail.com>
2023-08-18 22:20:08 +0200

    feat: add slider in about section

--- library/index.js
+++ library/index.js
@@ -82,23 +82,83 @@ function toChangeFavoritesOfSeason(season) {
 //slider
 const slider = document.querySelector('.about__slider');
 const slides = Array.from(document.querySelectorAll('.about__item'));
-const slideCount = slides.length;
 const pagination = document.querySelector('.about__carousel');
+const rightArrowButton = document.querySelector('.arrow-right');
+const leftArrowButton = document.querySelector('.arrow-left');
+let lastVisibleCirkle = Array.from(pagination.children).findLast((element) => element.offsetWidth > 0);
 
 let slideIndex = 0;
 
 function showNextSlide() {
-    slideIndex = (slideIndex + 1) % slideCount;
+    slideIndex += 1;
+    activeRightPagination();
     showSlider();
 }
 
 function showPreviousSlide() {
-    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    slideIndex -= 1;
+    activeLeftPagination();
     showSlider();
 }
 
-pagination.addEventListener('click', (event) => {
+function addActivePagination(button) {
+    button.classList.add('about__carousel__cirkle-active');
+    if (pagination.firstElementChild === button || lastVisibleCirkle === button) {
+        button.disabled = true;
+    }
+    checkLeftArrowButton();
+    checkRightArrowButton();
+}
+
+function checkLeftArrowButton() {
+    if (slideIndex === 0) {
+        leftArrowButton.disabled = true;
+    }
+}
+
+function checkRightArrowButton() {
+    if (slideIndex === +lastVisibleCirkle.innerText) {
+        rightArrowButton.disabled = true;
+    }
+}
+
+function activeRightPagination() {
+    const activePagination = document.querySelector('.about__carousel__cirkle-active');
+    const rightPagination = activePagination.nextElementSibling;
+    removeActivePagination();
+    addActivePagination(rightPagination);
+}
+
+function activeLeftPagination() {
+    const activePagination = document.querySelector('.about__carousel__cirkle-active');
+    const leftPagination = activePagination.previousElementSibling;
+    removeActivePagination();
+    addActivePagination(leftPagination);
+}
+
+function removeActivePagination() {
+    const activePagination = document.querySelector('.about__carousel__cirkle-active');
+    activePagination.classList.remove('about__carousel__cirkle-active');
+    activePagination.disabled = false;
+    leftArrowButton.disabled = false;
+    rightArrowButton.disabled = false;
+}
+
+rightArrowButton.addEventListener('click', (event) => {
     showNextSlide();
+})
+
+leftArrowButton.addEventListener('click', (event) => {
+    showPreviousSlide();
+})
+
+pagination.addEventListener('click', (event) => {
+    slideIndex = +event.target.innerText;
+    removeActivePagination();
+    addActivePagination(event.target);
+    checkLeftArrowButton();
+    checkRightArrowButton();
+    showSlider();
 });
 
 function showSlider() {
@@ -106,12 +166,22 @@ function showSlider() {
     activeSlide.classList.remove('about__item-active');
     slides.forEach((slide, index) => {
         if (index === slideIndex) {
-            slider.style.left = -slideIndex * 100 + 25 + 'px';
+            let slideWidth = slide.offsetWidth;
+            slider.style.marginLeft = -slideIndex * slideWidth - 25 * (slideIndex) + 'px';
             slide.classList.add('about__item-active');
-        } else {
-            
         }
     })
 }
+
+function checkLastPagination() {
+    lastVisibleCirkle = Array.from(pagination.children).findLast((element) => element.offsetWidth > 0);
+}
+
+window.addEventListener('resize', (event) => {
+    const activePagination = document.querySelector('.about__carousel__cirkle-active');
+    checkLastPagination();
+    removeActivePagination();
+    addActivePagination(activePagination);
+})
 
 showSlider();
